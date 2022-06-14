b8c9f195b3f924a5f53debc89109641252e454b4
huy160201 <duc4422@gmail.com>
2022-06-14 17:05:03 +0700

    get banners

---
+++ lumen_api/app/Http/Controllers/BannerController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Banner;
+class BannerController extends Controller
+{
+    
+    public function index()
+    {
+        $banners = Banner::all();
+
+        return response()->json($banners);
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show($id)
+    {
+        $banner = Banner::find($id);
+
+        return response()->json($banner);
+    }
+
+    
+    public function edit($id)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    
+    public function destroy($id)
+    {
+        //
+    }
+}
